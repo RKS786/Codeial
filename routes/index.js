7284d1039123e240ee821dcf78e7068062604e50
Ranik <er.rahul0314@gmail.com>
2023-10-28 21:38:01 +0530

    Final Commit

--- routes/index.js
+++ routes/index.js
@@ -5,7 +5,7 @@ const homeController = require('../controllers/home_controller');
 
 console.log('router loaded')
 
-router.get('/',homeController.home);
+router.get('/', homeController.home);
 router.use('/users', require('./users'));
 
 
