1d82464b3098dcb7d8a4abde2a63922b824b09fb
Ranik <er.rahul0314@gmail.com>
2023-11-24 13:23:30 +0530

    established mongoose connection

--- index.js
+++ index.js
@@ -1,26 +1,41 @@
+// Import the express library
 const express = require('express');
-const app =express();
-const port = 8000;
-const expressLayout = require('express-ejs-layouts');
 
+// Import the Mongoose connection object from the mongoose.js file in the config directory
+const db = require('./config/mongoose');
+
+// Create an instance of the express application
+const app = express();
+
+// Set the port for the server
+const port = 8000;
+
+// Import express-ejs-layouts for layout support
+const expressLayouts = require('express-ejs-layouts');
+
+// Serve static files from the 'assets' directory
 app.use(express.static('./assets'));
 
-app.use(expressLayout);
-// extract styles and scripts from sub pages into the layout
+// Use express-ejs-layouts middleware
+app.use(expressLayouts);
+
+// Extract styles and scripts from sub pages into the layout
 app.set('layout extractStyles', true);
 app.set('layout extractScripts', true);
 
-
-// use express router
+// Use express router defined in the 'routes' module
 app.use('/', require('./routes'));
 
-// set up the view engine
+// Set up the view engine as EJS
 app.set('view engine', 'ejs');
-app.set('views','./views');
 
+// Set the directory where views are located
+app.set('views', './views');
+
+// Start the server and listen on the specified port
 app.listen(port, function(err){
     if(err){
         console.log(`Error in running the server: ${err}`);
     }
     console.log(`Server is running on port: ${port}`);
-})
+});
